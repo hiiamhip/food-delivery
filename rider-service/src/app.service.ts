bd59976eba5c98b62e34cb7371ec93b991133e0c
Trần Tuấn Hiệp <trantuanhiep420@gmail.com>
2026-05-26 11:30:08 +0700

    rider-service: handle order-ready from kitchen-service

--- rider-service/src/app.service.ts
+++ rider-service/src/app.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { db } from './db/db';
+import { dispatches } from './db/schema';
+
+const RIDERS = ['Mike', 'Alex', 'Joe', 'Bright', 'Alice'];
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async dispatchRider(data: {
+    orderId: string;
+    customerName: string;
+    item: string;
+  }) {
+    // Pick a rider from a list;
+    const rider = RIDERS[Math.floor(Math.random() * RIDERS.length)];
+
+    const [dispatch] = await db
+      .insert(dispatches)
+      .values({
+        orderId: data.orderId,
+        customerName: data.customerName,
+        item: data.item,
+        riderStatus: 'dispatched',
+      })
+      .returning();
+
+    console.log('Dispatch saved with ID: ' + dispatch.id);
+    console.log(
+      rider +
+        ' is on the way with your item ' +
+        data.item +
+        ' for customer ' +
+        data.customerName,
+    );
   }
 }

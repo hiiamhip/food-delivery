c98b589d31bc0e47fa6ddb9e9499064f37f146a3
Trần Tuấn Hiệp <trantuanhiep420@gmail.com>
2026-05-26 11:18:40 +0700

    config rider-service as microservice app

--- rider-service/src/main.ts
+++ rider-service/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        url: ['amqp://guest:guest@localhost:5672'],
+        queue: 'rider_queue',
+        queueOptions: {
+          durable: process.env.NODE_ENV === 'production',
+        },
+      },
+    },
+  );
+
+  await app.listen();
+  console.log('🔥 Rider service is listening on rider_queue');
 }
-bootstrap();
+void bootstrap();
